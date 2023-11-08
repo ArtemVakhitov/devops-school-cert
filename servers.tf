a6a1184d94ac63a1391449cd3017b3ae8ae3e79e
Artem Vakhitov <artem.vakhitov@gmail.com>
2023-11-08 13:41:22 +0600

    Deduplicate instance creation code

--- servers.tf
+++ servers.tf
@@ -11,41 +11,16 @@ provider "yandex" {
   zone = "ru-central1-b"
 }
 
-resource "yandex_compute_instance" "build" {
-
-  name = "build"
-  hostname = "build"
-
-  zone = "ru-central1-b"
-  platform_id = "standard-v1"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8q5m87s3v0hmp06i5c"
-      size = 15
-    }
-  }
-
-  network_interface {
-    subnet_id = "e2lgv5mqm56n8fjkt37q"
-    nat = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_dsa.pub")}"
-  }
-
+locals {
+  names = ["build", "staging"]
 }
 
-resource "yandex_compute_instance" "staging" {
+resource "yandex_compute_instance" "terra" {
 
-  name = "staging"
-  hostname= "staging"
+  for_each = toset(local.names)
+
+  name = each.value
+  hostname = each.value
 
   zone = "ru-central1-b"
   platform_id = "standard-v1"
@@ -84,9 +59,9 @@ resource "local_file" "ansible_config" {
 resource "local_file" "ansible_inventory" {
   content  = <<-EOF
 								[build]
-								${yandex_compute_instance.build.network_interface.0.nat_ip_address}
+								${yandex_compute_instance.terra["build"].network_interface.0.nat_ip_address}
 								[staging]
-								${yandex_compute_instance.staging.network_interface.0.nat_ip_address}
+								${yandex_compute_instance.terra["staging"].network_interface.0.nat_ip_address}
 								[all:vars]
 								ansible_user=ubuntu
 								ansible_ssh_common_args="-o StrictHostKeyChecking=no -o ConnectionAttempts=20"
@@ -98,9 +73,9 @@ resource "local_file" "ansible_inventory" {
 }
 
 output "build_ip" {
-  value = yandex_compute_instance.build.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.terra["build"].network_interface.0.nat_ip_address
 }
 
 output "staging_ip" {
-  value = yandex_compute_instance.staging.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.terra["staging"].network_interface.0.nat_ip_address
 }
